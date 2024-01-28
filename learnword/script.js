6f79c7d6a4c9650c16ffbd159bba42cfddc7f193
Shinahin <info@shinahin.com>
2024-01-28 14:10:36 +0900

    Updata

--- learnword/script.js
+++ learnword/script.js
@@ -164,7 +164,6 @@ function inputform() {
       Incorrect++;
       if (val != responseJson[clickCount]["過去形"]) {
         alert(`不正解。正解は${responseJson[clickCount]["過去形"]}`);
-        clildcontent('.miss', `日本語: ${responseJson[clickCount]['日本語']}, 現在形: ${responseJson[clickCount]['現在形']}, 過去形: ${responseJson[clickCount]['過去形']}`, 'p');
       }
     } else if (val != responseJson[clickCount]["過去形"]) {
       alert(`不正解。正解は${responseJson[clickCount]["過去形"]}`);
